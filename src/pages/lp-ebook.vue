e5f6f6e9f206ee997955865f57dfbec8ca43b6a9
DhCavalcante777 <dh_cavalcante@hotmail.com>
2025-04-13 22:06:08 -0300

    adicao de secao de compra + envio de email

--- src/pages/lp-ebook.vue
+++ src/pages/lp-ebook.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container-fluid">
-    <div class="row background-ebook">
-      <div class="col-10 col-sm-6 mx-auto">
+    <div class="row background-ebook pb-5">
+      <div class="col-10 col-sm-6 mx-auto ">
         <p class="text-white text-center title-ebook">
           <strong>Quer ter uma vida saudável ?</strong>
         </p>
-        <p class="text-white text-center title-ebook"> A susu vai te ajudar. Baixei o e-book, coma bem e de forma saudável.</p>
+        <p class="text-white text-center title-ebook">
+          A Susu vai te ajudar. Baixei o e-book gratuito, coma bem e de forma saudável.
+        </p>
       </div>
       <div class="col-10 mx-auto">
         <img
@@ -14,50 +16,6 @@
           alt="Imagem de fundo"
         />
       </div>
-      <div class="col-10 col-sm-6 mx-auto">
-        <p class="text-white text-center title-ebook">
-          <strong>Quer receber o conteúdo gratuito?</strong>
-        </p>
-      </div>
-      <div class="d-grid gap-2 col-10 mx-auto pb-5">
-        <a
-          href="/e-book/cozinharAlegraAAlma.pdf"
-          class="btn btn-primary btn-lg button-sl"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Receber conteúdo gratuito
-        </a>
-      </div>
-      <!-- <div class="col-12 col-sm-6">
-        <div class="mb-3">
-          <label for="fullName" class="form-label"
-            ><strong>Nome Completo*</strong></label
-          >
-          <input
-            type="email"
-            class="form-control"
-            id="fullName"
-            placeholder="Suelen Coelho"
-            v-model="fullName"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="email" class="form-label"><strong>Email*</strong></label>
-          <input
-            type="email"
-            class="form-control"
-            id="email"
-            placeholder="susu.scfisio@gmail.com"
-            v-model="email"
-          />
-        </div>
-        <div class="d-grid gap-2">
-          <button class="btn btn-primary" type="button" @click="sendEmail">
-            Receber conteúdo gratuito
-          </button>
-        </div>
-      </div> -->
     </div>
     <div class="row ebook-content pb-5">
       <div class="col-10 col-sm-6 mx-auto">
@@ -65,37 +23,51 @@
           <strong>O que você vai encontrar no e-book?</strong>
         </p>
         <div class="row">
-          <div class="col-12 col-sm-6 d-flex justify-content-center align-items-center">
+          <div
+            class="col-12 col-sm-6 d-flex justify-content-center align-items-center"
+          >
             <img
               src="../assets/image/e-book/ic-certinho.png"
               class="icon-content"
               alt="Imagem de fundo"
             />
-            <p class="text-center description-content">Dicas de como cozinhar de forma saudável.</p>
+            <p class="text-center description-content">
+              Dicas de como cozinhar de forma saudável.
+            </p>
           </div>
         </div>
         <div class="row">
-          <div class="col-12 col-sm-6 d-flex justify-content-center align-items-center">
+          <div
+            class="col-12 col-sm-6 d-flex justify-content-center align-items-center"
+          >
             <img
               src="../assets/image/e-book/ic-certinho.png"
               class="icon-content"
               alt="Imagem de fundo"
             />
-            <p class="text-center description-content">Receitas práticas e saudáveis para o seu dia a dia.</p>
+            <p class="text-center description-content">
+              Receitas práticas e saudáveis para o seu dia a dia.
+            </p>
           </div>
         </div>
         <div class="row">
-          <div class="col-12 col-sm-6 d-flex justify-content-center align-items-center">
+          <div
+            class="col-12 col-sm-6 d-flex justify-content-center align-items-center"
+          >
             <img
               src="../assets/image/e-book/ic-certinho.png"
               class="icon-content"
               alt="Imagem de fundo"
             />
-            <p class="text-center description-content">Dicas de como emagrecer com saúde.</p>
+            <p class="text-center description-content">
+              Dicas de como emagrecer com saúde.
+            </p>
           </div>
         </div>
         <div class="row">
-          <div class="col-12 col-sm-6 d-flex justify-content-center align-items-center">
+          <div
+            class="col-12 col-sm-6 d-flex justify-content-center align-items-center"
+          >
             <img
               src="../assets/image/e-book/ic-certinho.png"
               class="icon-content"
@@ -106,47 +78,149 @@
         </div>
       </div>
     </div>
+    <div class="col-12 col-sm-6 mx-auto gift-ebook pb-5">
+      <p class="text-center gift-ebook-title">
+        <strong>Este presente é para você!</strong>
+      </p>
+      <p class="text-center gift-ebook-description">
+        Baixe agora o nosso e-book gratuito e transforme sua alimentação.
+      </p>
+      <div class="d-grid gap-2">
+        <a
+          href="/e-book/cozinharAlegraAAlma.pdf"
+          class="btn btn-primary btn-lg button-sl"
+          target="_blank"
+          rel="noopener noreferrer"
+          onclick="window.location.href='#treatments'"
+        >
+          Baixar E-book Gratuito
+        </a>
+        <span class="text-center attention-text">Não perca a promoção abaixo</span>
+      </div>
+    </div>
+    <div
+      id="ebook-promotion"
+      class="row ebook-promotion pb-5"
+    >
+      <div class="col-12 col-sm-6 mx-auto">
+        <p class="text-center ebook-promotion-title text-white">
+          <strong>Promoção de Inauguração!</strong>
+        </p>
+        <p class="text-center ebook-promotion-description text-white">
+          Aproveite a oferta especial de lançamento:
+          <strong>De R$ 29,90 por apenas R$ 9,90!</strong>
+        </p>
+        <div class="countdown-timer text-center">
+          <p class="text-danger">
+            Oferta válida por tempo limitado!
+          </p>
+        </div>
+
+        <div class="col-12 col-sm-6 mx-auto mt-4">
+          <p class="text-center ebook-promotion-title text-white">
+            <strong>Óleos essenciais - Aprenda a usar em casa</strong>
+          </p>
+          <p class="text-center ebook-promotion-description text-white">
+            Este e-book exclusivo foi criado para ajudar você a entender como os óleos essenciais podem transformar sua saúde e bem-estar. Aprenda:
+          </p>
+          <ul class="benefits-list text-white">
+            <li>Como usar óleos essenciais no dia a dia.</li>
+            <li>Benefícios para a saúde física e emocional.</li>
+            <li>Receitas práticas para aromaterapia.</li>
+            <li>Dicas para escolher óleos de alta qualidade.</li>
+          </ul>
+        </div>
+
+        <div class="d-grid gap-2">
+          <button
+            class="btn btn-success btn-lg"
+            type="button"
+            @click="redirectToPayment"
+          >
+            Comprar agora por R$ 9,90
+          </button>
+        </div>
+      </div>
+    </div>
+    
+    <div id="payment-proof" class="row payment-proof-section pb-5">
+      <div class="col-12 col-sm-6 mx-auto">
+        <p class="text-center payment-proof-title">
+          <strong>Envie seu comprovante de pagamento</strong>
+        </p>
+        <p class="text-center payment-proof-description">
+          Preencha os campos abaixo e clique no botão para enviar o e-mail. Não se esqueça de anexar o comprovante no seu e-mail.
+        </p>
+          <div class="mb-3">
+            <label for="fullName" class="form-label"><strong>Nome Completo*</strong></label>
+            <input
+              type="text"
+              class="form-control"
+              id="fullName"
+              placeholder="Seu nome completo"
+              v-model="fullName"
+              required
+            />
+          </div>
+          <div class="mb-3">
+            <label for="email" class="form-label"><strong>Email*</strong></label>
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              placeholder="Seu e-mail"
+              v-model="email"
+              required
+            />
+          </div>
+          <div class="mb-3">
+            <label for="message" class="form-label"><strong>Mensagem (opcional)</strong></label>
+            <textarea
+              class="form-control"
+              id="message"
+              rows="3"
+              placeholder="Escreva uma mensagem, se desejar"
+              v-model="message"
+            ></textarea>
+          </div>
+          <div class="d-grid gap-2">
+            <button type="submit" class="btn btn-primary btn-lg" @click="sendMailto">
+              Enviar Comprovante
+            </button>
+          </div>
+      </div>
+    </div>
     <Footer />
   </div>
 </template>
 <script setup>
 import { ref } from "vue";
-import emailjs from "@emailjs/browser";
 import Footer from "@/components/footer.vue";
 
 const fullName = ref("");
 const email = ref("");
+const message = ref("");
 
-const sendEmail = () => {
+const sendMailto = () => {
   if (!fullName.value || !email.value) {
-    alert("Por favor, preencha todos os campos.");
+    alert("Por favor, preencha todos os campos obrigatórios.");
     return;
   }
 
-  const templateParams = {
-    title:'E-book gratis',
-    name: fullName.value,
-    message:'email: ' + email.value,
-  };
+  const subject = encodeURIComponent("Comprovante de Pagamento");
+  const body = encodeURIComponent(
+    `Nome: ${fullName.value}\nEmail: ${email.value}\nMensagem: ${message.value || "Sem mensagem"}\n\nPor favor, anexe o comprovante de pagamento ao enviar este e-mail.`
+  );
 
-  emailjs
-    .send(
-      "service_vp92mbr", // Substitua pelo seu Service ID
-      "template_fx3x3ba", // Substitua pelo seu Template ID
-      templateParams,
-      "ZXQkSnHNlcXsdIugM"
-    )
-    .then(
-      (response) => {
-        alert("E-mail enviado com sucesso!");
-        console.log("SUCCESS!", response.status, response.text);
-      },
-      (error) => {
-        alert("Erro ao enviar o e-mail. Tente novamente.");
-        console.error("FAILED...", error);
-      }
-    );
+  const mailtoLink = `mailto:susu.scfisio@gmail.com?subject=${subject}&body=${body}`;
+  window.location.href = mailtoLink;
 };
+
+const redirectToPayment = () => {
+  window.location.href =
+    "https://pay.infinitepay.io/sldermafisio/Ri0x-KYJJmgd4J-9,90";
+};
+
 </script>
 <style scoped>
 .container-fluid {
@@ -198,5 +272,76 @@ const sendEmail = () => {
 .description-content {
   font-size: 20px;
   margin-bottom: 0px;
+}
+
+.ebook-promotion {
+  background-color: #313131;
+  padding: 20px;
+  border-radius: 5px;
+}
+
+.ebook-promotion-title {
+  font-size: 25px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.ebook-promotion-description {
+  font-size: 20px;
+  margin-bottom: 20px;
+}
+
+.benefits-list {
+  list-style: none;
+  padding: 0;
+  margin: 20px 0;
+  font-size: 18px;
+}
+
+.benefits-list li {
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+}
+
+.benefits-list li::before {
+  content: "✔";
+  color: #28a745;
+  margin-right: 10px;
+  font-weight: bold;
+}
+
+.gift-ebook {
+  background-color: #fff;
+  padding: 20px;
+}
+
+.gift-ebook-title {
+  font-size: 25px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  color: #fa5894;
+}
+
+.gift-ebook-description {
+  font-size: 20px;
+  margin-bottom: 20px;
+  color: #fa5894;
+}
+
+.payment-proof-section {
+  background-color: #f6daeb;
+  padding: 20px;
+}
+
+.payment-proof-title {
+  font-size: 25px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.attention-text {
+  font-size: 15px;
+  color: red;
 }
 </style>
